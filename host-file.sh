d4ced2c7763467e0e470c2c77ff28787474bb2d9
Kyle Wilson <kwilson@ooyala.com>
2015-08-18 18:07:23 -0700

    fixed host script and folder

--- host-file.sh
+++ host-file.sh
@@ -1,5 +1,9 @@
-for f in /Users/kwilson/tstflt-alias/*; do
+HOSTDIR="/Users/kwilson/work/ooyala/ELK/atlantis-logstash-repos/logstash-atlantis-remote-control/hosts"
+
+for f in $HOSTDIR/*; do
 	echo "Copying ${f}"
+	echo "Remote: $(cat ${f})"
 	filename=$(basename "$f")
-	scp "$f" "root@$(cat ${f}):/root/${filename}"
+	scp "${f}" "root@$(cat ${f}):/root/atlantis-analytics"
+	ssh "root@$(cat ${f})" "cd /root/atlantis-analytics; echo '${filename}' > localname; mv ${filename} address"
 done
